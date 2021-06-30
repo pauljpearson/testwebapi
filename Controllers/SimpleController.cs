754b13cf6adc0424cab59c1457c2830e18ef61b4
Paul Pearson <paul.pearson@anana.com>
2021-06-30 14:43:19 +0100

    v1 files

---
+++ Controllers/SimpleController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace TestWebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SimpleController : ControllerBase
+    {
+
+
+        private readonly ILogger<SimpleController> _logger;
+
+        public SimpleController(ILogger<SimpleController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            string version = "v1";
+            return $"{version} {System.Environment.MachineName}";
+        }
+    }
+}
